6c27fe2f58407752310e53320518b22275e5c4d0
Vamsi <bvamsi489@gmail.com>
2023-07-05 19:01:16 +0530

    day 3

--- payment/payment.js
+++ payment/payment.js
@@ -2,10 +2,101 @@
 
 let getOTP = document.getElementById("getOTPButton");
 
-getOTP.addEventListener("click", function() {
-    alert("OTP is 0000");
-    validateOTP();
+let verifyButton = document.createElement("button");
+verifyButton.setAttribute("id", "verifyButton")
+verifyButton.textContent = "Verify & Continue";
+//to verify the details STARTED
+
+verifyButton.addEventListener("click", function() {
+    let enteredOTP = document.getElementById("otp").value;
+    if(enteredOTP === "0000") {
+        alert("success");
+        changeUiAfterContinue();
+        payment();
+    }
+    else {
+        alert("Wrong OTP. Use 0000");
+    }
 })
+//to verify the details ENDED
+
+function changeUiAfterContinue() {
+    document.getElementById("dummyPara").innerText = null;
+    document.getElementById("headingDiv").innerText = "Your Details";
+    document.getElementById("headingDiv").style.fontWeight = "bold";
+    // let name = document.getElementById("name");
+    // let email = document.getElementById("email");
+    // let mobile = document.getElementById("mobile");
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let mobile = document.getElementById("mobile").value;
+    let nameDiv = document.createElement("div");
+    nameDiv.setAttribute("id", "nameDiv");
+    let emailDiv = document.createElement("div");
+    emailDiv.setAttribute("id", "emailDiv");
+    
+    let mobileDiv = document.createElement("div");
+    mobileDiv.setAttribute("id", "mobileDiv");
+    nameDiv.append(name);
+    emailDiv.append(email);
+    mobileDiv.append(mobile);
+    let showDetailsDiv = document.createElement("div");
+    showDetailsDiv.setAttribute("id", "showDetailsDiv");
+    let modifyButtonDiv = document.createElement("div");
+    modifyButtonDiv.setAttribute("id", "modifyButtonDiv");
+    let modifyButton = document.createElement("button");
+    modifyButton.setAttribute("id", "modifyButton");
+    modifyButton.textContent = "Modify";
+    modifyButtonDiv.append(modifyButton);
+    // document.getElementById("formDiv").append(modifyButton);
+    showDetailsDiv.append(nameDiv, emailDiv, mobileDiv, modifyButtonDiv);
+    document.getElementById("formDiv").innerHTML = "";
+    document.getElementById("formDiv").append(showDetailsDiv);
+}
+
+getOTP.addEventListener("click", function() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let mobile = document.getElementById("mobile").value;
+    let emailValidity = validateEmailInput(email);
+    let mobileValidity = validateMobileInput(mobile);
+console.log(emailValidity, mobileValidity);
+    if(emailValidity == true && mobileValidity==true) {
+        alert("OTP is 0000");
+        validateEmailInput(email);
+        validateMobileInput(mobile);
+        document.getElementById("verifyButtonDiv").innerHTML = null;
+        document.getElementById("verifyButtonDiv").append(verifyButton);
+        validateOTP();
+    }
+    console.log(name + " " + email + " " + mobile);
+})
+
+
+function validateEmailInput(email) {
+    let emails = document.getElementById("email");
+    
+    if (emails.checkValidity()) {
+      return true;
+    } else {
+      alert('Your Email Address is invalid!');
+      return false;
+    }
+  }
+
+function validateMobileInput(mobile) {
+    
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let mobiles = document.getElementById("mobile");
+    
+    if (mobiles.checkValidity() && mobiles.value.length === 10) {
+      return true;
+    } else {
+      alert('Your Mobile Number is invalid!');
+      return false;
+    }
+  }
 
 function validateOTP() {
     let enterOTP = document.createElement("input");
@@ -21,19 +112,7 @@ function validateOTP() {
 
 //to get OTP FINISHED
 
-//to verify the details STARTED
-let verifybtn = document.getElementById("verifyButton");
-verifybtn.addEventListener("click", function() {
-    let enteredOTP = document.getElementById("otp").value;
-    if(enteredOTP === "0000") {
-        alert("success");
-        payment();
-    }
-    else {
-        alert("Wrong OTP. Use 0000");
-    }
-})
-//to verify the details ENDED
+
 
 //to accept paymetn STARTED
 function payment() {
